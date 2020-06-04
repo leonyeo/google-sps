ebe7347a74cd1e1e768a5ddcb87d8f006bab26a9
Leon Yeo <yeolxleon@gmail.com>
2020-06-04 21:03:13 +0800

    Add header section

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,38 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+window.onload = function() {
+    // Find all elements to add the typing animation
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i < elements.length; i++) {
+        var text = elements[i].getAttribute('data-text');
+        var time = elements[i].getAttribute('data-time');
+        if (text) {
+            txtRotate(elements[i], JSON.parse(text), parseInt(time), 0, true);
+        } 
+    }
+}
+
+function txtRotate(elem, text, time, index, isAscending) {
+    // Function to 'type' text, wait for time, backspace, then move on to next index in text array and repeat
+    if (isAscending) {
+        elem.innerHTML = text[index].slice(0, elem.innerHTML.length + 1);
+        if (elem.innerHTML.length == text[index].length) {
+            // If word has been displayed, pause for specified time, the backspace word
+            setTimeout(txtRotate, time, elem, text, time, index, false);
+            return;
+        }
+    }
+    else if (!isAscending) {
+        elem.innerHTML = text[index].slice(0, elem.innerHTML.length - 1);
+        if (elem.innerHTML.length == 0) {
+            // If word has been backspaced, move onto next index
+            index = (index + 1) % text.length;
+            setTimeout(txtRotate, 500, elem, text, time, index, true);
+            return;
+        }
+    }
+    // Else continue on to type/backspace next word
+    setTimeout(txtRotate, 150, elem, text, time, index, isAscending);
+}
